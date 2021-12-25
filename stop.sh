e7f1b5938a0734a23545488fbb28c9d679aab96f
Vincenzo <enzo.mar@gmail.com>
2021-12-25 15:33:59 +0100

    fix incompatibilities between linux/osx

--- stop.sh
+++ stop.sh
@@ -2,9 +2,9 @@
 
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	ps -aux | grep pollicino | grep -v grep | awk '{print "kill -9 " $2}' | sh
-	ps -aux | grep broker | grep -v grep | awk '{print "kill -9 " $2}' | sh
-	ps -aux | grep mosquitto | grep -v grep | awk '{print "kill -9 " $2}' | sh
+	ps -aux | grep pollicino | grep -v grep | awk '{print "sudo kill -9 " $2}' | sh
+	ps -aux | grep broker | grep -v grep | awk '{print "sudo kill -9 " $2}' | sh
+	ps -aux | grep mosquitto | grep -v grep | awk '{print "sudo kill -9 " $2}' | sh
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	ps -ef | grep pollicino | grep -v grep | awk '{print "kill -9 " $2}' | sh
