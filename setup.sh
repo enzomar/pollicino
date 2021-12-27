f08e3707a80a35dc24d0808b2f36f75f055d173c
Vincenzo MARAFIOTI <vincenzo.marafioti@amadeus.com>
2021-12-27 08:01:25 +0100

    add mosquitto for osx and linux in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "# MOSQUITTO"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	sudo apt-get install mosquitto
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	brew install mosquitto
+fi
+
+echo "# VIRTUALENV"
 virtualenv -p python3 venv
 source venv/bin/activate
+
+echo "# REQUIREMENTS"
 pip install -r requirements.txt
