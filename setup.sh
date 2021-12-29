e6343dceba55a9cb88de2ff02172c38c2668f653
Vincenzo MARAFIOTI <vincenzo.marafioti@amadeus.com>
2021-12-29 09:34:09 +0100

    smarter setup (if) + state role

--- setup.sh
+++ setup.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
 
-echo "# MOSQUITTO"
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	sudo apt-get install mosquitto
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	brew install mosquitto
-fi
 
-echo "# VIRTUALENV"
-pip3 install virtualenv 
-python -m virtualenv -p python3 venv
-source venv/bin/activate
+function create_virtualenv {
+	echo "# VIRTUALENV"
+	if [ -d venv ]; then
+		echo "Skipping beacause it is already created"
+	else
+		pip3 install virtualenv 
+		python -m virtualenv -p python3 venv
+	fi
+	source venv/bin/activate
+}
 
-echo "# REQUIREMENTS"
-pip3 install -r requirements.txt
+function install_mosquitto {
+	echo "# MOSQUITTO"
+
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		if apt list mosquitto --installed | grep -q mosquitto; then
+			echo "Skipping beacause it is already installed"
+		else
+			sudo apt-get install mosquitto
+		fi
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		if brew list --formula -1 | grep -q mosquitto; then
+			echo "Skipping beacause it is already installed"
+		else
+			brew install mosquitto
+		fi
+	fi
+}
+
+function install_requirements {
+	echo "# REQUIREMENTS"
+	if [[ -z "$VIRTUAL_ENV" ]]
+	then
+		echo "Skipping beacause virtualenv is not active"
+	else
+		pip3 install -r requirements.txt
+	fi
+
+}
+
+
+
+install_mosquitto
+create_virtualenv
+install_requirements
+
