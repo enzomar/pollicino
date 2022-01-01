3dd0e095470ebcf9a5498b39a1ee2a6dbdffcb5a
Vincenzo <enzo.mar@gmail.com>
2022-01-01 19:13:15 +0100

    automatic load config from host name

--- pollicino.py
+++ pollicino.py
@@ -9,6 +9,8 @@ from pollicino.servos import servos
 from pollicino.helpers import broker
 import argparse
 import logging
+import socket
+
 
 LOG_FORMAT_1 = '[%(asctime)s - %(process)d - %(processName)s - %(levelname)s - %(pathname)s:%(lineno)d] %(message)s'
 LOG_FORMAT_2 = '[%(asctime)s] p%(process)s %(pathname)s:%(lineno)d} %(levelname)s - %(message)s , %m-%d %H:%M:%S'
@@ -17,44 +19,57 @@ LOG_FORMAT_2 = '[%(asctime)s] p%(process)s %(pathname)s:%(lineno)d} %(levelname)
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT_1)
 
 
-DEFAUL_TOPOLOGY_FILE = "pollicino.yaml"
+DEFAUL_CONFIG_FILE = "config/pollicino.yaml"
+AUTO_CONFIG = 'config/{0}.yaml'.format(socket.gethostname())
 
 
 
 def parse_input():
 	parser = argparse.ArgumentParser(allow_abbrev=False)
 	parser.add_argument('-m','--mode', action='store', choices=['servos', 'sensors', 'ctrl'])
-	parser.add_argument('-t','--topology', action='store', default=DEFAUL_TOPOLOGY_FILE)
 	parser.add_argument('-b','--broker', action='store', default='0.0.0.0')
+	
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('-c','--config', action='store', default=DEFAUL_CONFIG_FILE)
+	group.add_argument('-a','--autoconfig', action='store_true')
 
 	args = parser.parse_args()
-	return args.mode, args.topology, args.broker
+
+	config = args.config
+	if args.autoconfig:
+		config = AUTO_CONFIG
+	
+	return args.mode, config, args.broker
 
 
-def run(mode, topology, broker_host):
+def run(mode, config, broker_host):
+	logging.info("MODE: {0}".format(mode))
+	logging.info("CONFIG: {0}".format(config))
+	logging.info("BROKER HOST: {0}".format(broker_host))
+
 	broker.Broker().set_host(broker_host)
 
 	if mode == "servos":
 		# subscribe for each topic and apply the relative action
 		logging.info("Run Servos")
-		servos.run(topology) 
+		servos.run(config) 
 
 	if mode == "sensors":
 		# multi threads: polls sensor status and 
 		# publish the status via MQTT protocol to the MQTT broker 
 		# one topic per sensor type
 		logging.info("Run Sensors")
-		sensors.run(topology)
+		sensors.run(config)
 
 	if mode == "ctrl":
 		# multi threads: polls sensor status and 
 		# publish the status via MQTT protocol to the MQTT broker 
 		# one topic per sensor type
 		logging.info("Run Controller")
-		controller.run(topology)
+		controller.run(config)
 
 
 if __name__ == "__main__":
-	mode, topology, broker_host = parse_input()
-	run(mode, topology, broker_host)
+	mode, config, broker_host = parse_input()
+	run(mode, config, broker_host)
 
