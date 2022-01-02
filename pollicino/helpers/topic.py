8cc1a7a2f999af5be24deaa27b36af8c56dbc194
Vincenzo <enzo.mar@gmail.com>
2022-01-02 11:46:32 +0100

    ux

--- pollicino/helpers/topic.py
+++ pollicino/helpers/topic.py
@@ -25,15 +25,14 @@ def status_sub(dev_id, dev_name, category, sector):
 def cmd_pub(dev_id, dev_name, category, sector):
     return PATTERN_CMD_PUB.format(
         sector=sector,
-        mac=gma(),
         category=category,
         dev_name=dev_name,
         dev_id=dev_id)
 
 
 def cmd_sub(dev_id, dev_name, category, sector):
-    return PATTERN_CMD_SUB.format(sector=sector, 
-        mac=gma(), 
+    return PATTERN_CMD_SUB.format(
+        sector=sector, 
         category=category, 
         dev_name=dev_name,
         dev_id=dev_id)
@@ -41,4 +40,5 @@ def cmd_sub(dev_id, dev_name, category, sector):
 
 def extract(topic):
     splitted = topic.split('/')
+    # sector, category, sensor_type, sensor_id
     return splitted[0], splitted[1], splitted[2], splitted[3]
